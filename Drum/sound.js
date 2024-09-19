f75991bc809275576cb1907d753a55c1ee34a83a
NikitaSkorohov <nikitaskoroho14@gmail.com>
2024-09-19 13:10:22 +0300

    Кнопки со звуками

--- Drum/sound.js
+++ Drum/sound.js
@@ -1,17 +1,16 @@
 $(function() {
-
     $(this).keydown(function(event) {
         
         var key = $(this).find('.key[data-key='+event.which+']');
 
-        var audio = $(this).find('.audio[data-key='+event.which+']')[0];
+        var audio = $(this).find('audio[data-key='+event.which+']')[0];
 
         key.toggleClass('playing');
 
         if (!audio) return;
 
         audio.play();
-
+        console.log('hello world');
         audio.currentTime = 0;
     });
 
