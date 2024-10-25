a9213c17f0c00a404bd1504bde3d67c1d88973b1
NikitaSkorohov <nikitaskoroho14@gmail.com>
2024-10-25 11:18:44 +0300

    Добавляем TODO лист созданный при помощи React

---
+++ react1/precache-manifest.38475fd1b82b532f5b678a52ce62142a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "475df079dc3457b33970",
+    "url": "/react1/static/css/main.42df0be4.chunk.css"
+  },
+  {
+    "revision": "475df079dc3457b33970",
+    "url": "/react1/static/js/main.475df079.chunk.js"
+  },
+  {
+    "revision": "a37507bfb71d50f0a0ae",
+    "url": "/react1/static/css/1.e9e817cd.chunk.css"
+  },
+  {
+    "revision": "a37507bfb71d50f0a0ae",
+    "url": "/react1/static/js/1.a37507bf.chunk.js"
+  },
+  {
+    "revision": "a74937258285e9ed0a2e",
+    "url": "/react1/static/js/runtime~main.a7493725.js"
+  },
+  {
+    "revision": "477a6274ceaede257c2e5253ee967a88",
+    "url": "/react1/index.html"
+  }
+];
